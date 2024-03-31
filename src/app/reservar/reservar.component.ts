baa3f7d4cd33dc4517f47a891557e649bb51e780
Maria Alvarez <maria.alvarez9296@gmail.com>
2024-03-31 21:15:37 +0200

    -

--- src/app/reservar/reservar.component.ts
+++ src/app/reservar/reservar.component.ts
@@ -12,27 +12,11 @@ export class ReservarComponent  implements OnInit {
 
   ngOnInit() {}
 
-  nombreAlumno: string = ''; // Variable para almacenar el nombre del alumno
-  alumnos: string[] = []; // Arreglo para almacenar los nombres de los alumnos
+  nombreAlumno: string = ''; 
   tipoClase: string = '';
   fechaSeleccionada: string = '';
 
-
-
-  // Método para agregar un alumno a la lista
-  agregarAlumno() {
-    if (this.nombreAlumno.trim() !== '') {
-      this.alumnos.push(this.nombreAlumno);
-      this.nombreAlumno = ''; // Limpiamos el campo de texto después de agregar el alumno
-    }
-  }
-  agregarCampo() {
-    this.alumnos.push(''); // Agregar un nuevo campo vacío
-  }
-  // Método para guardar la reserva
   guardar() {
-    // Aquí puedes implementar la lógica para guardar la reserva
-    console.log('Reserva guardada:', this.alumnos);
-  }
-
+    //lógica para guardar la reserva
+    
 }
